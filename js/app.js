6d22f97320334155d66f05e17a31f5f93cb48f91
IlliaKit <kitilla7@gmail.com>
2023-05-12 21:16:03 +0200

    end

--- js/app.js
+++ js/app.js
@@ -13,4 +13,56 @@ if (ScrollTrigger.isTouch !== 1) {
     smooth: 1.5,
     effects: true,
   });
+
+  gsap.fromTo(
+    ".hero-section",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".hero-section",
+        start: "center",
+        end: "1500",
+        scrub: true,
+      },
+    }
+  );
+
+  let itemsL = gsap.utils.toArray(".gallery__left .gallery__item");
+
+  itemsL.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { x: -100, opacity: 0 },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-600",
+          end: "-100",
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  let itemsR = gsap.utils.toArray(".gallery__right .gallery__item");
+
+  itemsR.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { x: 100, opacity: 0 },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-600",
+          end: "-100",
+          scrub: true,
+        },
+      }
+    );
+  });
 }
